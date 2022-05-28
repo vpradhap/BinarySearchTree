25d61908ddb53bd5a5f190af339960642dde3fbf
vpradhap <pradhapvenkat@gmail.com>
2022-05-28 09:05:47 +0530

    Added : Creating binary search tree with 56,30 and 70 elements

---
+++ BST.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BST<T> where T : IComparable
+    {
+        
+        public T nodeData { get; set; }
+        public BST<T> leftTree { get; set; }
+
+        public BST<T> rightTree { get; set; }
+
+        public BST(T data)
+        {
+            nodeData = data;
+            leftTree = null;
+            rightTree = null;
+        }
+
+        public void Insert(T item)
+        {
+            T CurrNodeVal = nodeData;
+            if ((CurrNodeVal.CompareTo(item)) > 0)
+            {
+                if (leftTree == null)
+                {
+                    leftTree = new BST<T>(item);
+                }
+                else
+                {
+                    leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (rightTree == null)
+                {
+                    rightTree = new BST<T>(item);
+                }
+                else
+                {
+                    rightTree.Insert(item);
+                }
+            }
+        }
+
+        public void Inorder()
+        {
+            if (leftTree != null)
+            {
+                leftTree.Inorder();
+            }
+            Console.Write(nodeData + " ");
+            if (rightTree != null)
+            {
+                rightTree.Inorder();
+            }
+        }
+        public void Preorder()
+        {
+            Console.Write(nodeData + " ");
+            if (leftTree != null)
+            {
+                leftTree.Preorder();
+            }
+            if (rightTree != null)
+            {
+                rightTree.Preorder();
+            }
+        }
+        public void Postorder()
+        {
+            if (leftTree != null)
+            {
+                leftTree.Postorder();
+            }
+            if (rightTree != null)
+            {
+                rightTree.Postorder();
+            }
+            Console.Write(nodeData + " ");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿
+﻿using BinarySearchTree;
+
 Console.WriteLine("\n\t\tBinary Search Tree Program");
+BST<int> tree = new BST<int>(56);
+
+int flag = 0;
+while (flag == 0)
+{
+    Console.WriteLine("\n1 - Creating binary search tree");
+    Console.Write("\nYour Pick : ");
+    int pick = Convert.ToInt32(Console.ReadLine());
+    switch (pick)
+    {
+        case 1:
+            tree.Insert(30);
+            tree.Insert(70);
+            Console.WriteLine("\nBinary search tree sequence");
+            Console.Write("\nInoder sequence    --> ");
+            tree.Inorder();
+            Console.Write("\nPreoder sequence   --> ");
+            tree.Preorder();
+            Console.Write("\nPostorder sequence --> ");
+            tree.Postorder();
+            Console.WriteLine();
+            break;
+        default:
+            flag = 1;
+            Console.WriteLine("\nEnter valid number");
+            break;
+    }
+}
